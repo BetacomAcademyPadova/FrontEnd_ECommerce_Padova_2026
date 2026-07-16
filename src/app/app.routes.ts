effe77be718b5176eae39912ebe02af68dda4c04
simonestecca2003-ux <simonestecca2003@gmail.com>
2026-07-16 17:56:04 +0200

    Secondo commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,6 @@ import { Routes } from '@angular/router';
 import { Dashboard } from './componenti/dashboard/dashboard';
 import { Home } from './componenti/home/home';
 import { GestioneUtente } from './componenti/gestione-utente/gestione-utente';
-import { GestioneVeicoli } from './componenti/gestione-veicoli/gestione-veicoli';
 import { autentificateGuard } from './auth/autentificate-guard';
 import { adminGuard } from './auth/admin-guard';
 import { Carello } from './componenti/carello/carello';
@@ -14,7 +13,6 @@ export const routes: Routes = [
             { path: 'home', component: Home },
             { path: 'carello', component: Carello , canActivate:[autentificateGuard]},
             { path: 'utente', component: GestioneUtente , canActivate:[autentificateGuard, adminGuard]},
-            { path: 'veicoli', component: GestioneVeicoli , canActivate:[autentificateGuard, adminGuard]},
         ]
     },
 
