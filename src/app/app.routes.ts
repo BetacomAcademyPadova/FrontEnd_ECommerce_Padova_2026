5656530b286e38065042949fbafd56173e99f949
Vadim <49763091+Vrudim@users.noreply.github.com>
2026-07-21 17:27:34 +0200

    ripristino gesti utenti

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,8 +9,6 @@ import { GestioneProdotti } from './componenti/gestione-prodotti/gestione-prodot
 import { Prodotti } from './componenti/prodotti/prodotti';
 import { Profilo } from './componenti/profilo/profilo';
 import { Login } from './dialogs/login/login';
-import { Checkout } from './componenti/checkout/checkout';
-import { CheckoutResult } from './componenti/checkout-result/checkout-result';
 
 export const routes: Routes = [
     { path:'', redirectTo:'dash', pathMatch:'full'},
@@ -22,8 +20,6 @@ export const routes: Routes = [
             { path: 'prodotti', component: Prodotti }, 
             { path: 'profilo', component: Profilo },  
             { path: 'utente', component: GestioneUtente , /*canActivate:[autentificateGuard, adminGuard]*/},
-            { path: 'checkout', component: Checkout },
-            { path: 'checkout-result', component: CheckoutResult },
         ]
     },
 ];
