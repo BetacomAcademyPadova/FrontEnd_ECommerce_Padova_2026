b95b04868068cfdc8d721884e56cdc2380d13518
simonestecca2003-ux <simonestecca2003@gmail.com>
2026-07-23 09:26:02 +0200

    GestioneUtente e Login

--- src/app/componenti/gestione-utente/gestione-utente.ts
+++ src/app/componenti/gestione-utente/gestione-utente.ts
@@ -7,8 +7,8 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatListModule } from "@angular/material/list";
 import { SceltaUpdateUtente } from '../../dialogs/scelta-update-utente/scelta-update-utente';
 import { UtilitiesServices } from '../../services/utilities-services';
-import { UtenteServices } from '../../services/utente-services';
 import { MatSelectModule } from '@angular/material/select';
+import { UtenteServices } from '../../services/user-services';
 
 @Component({
   selector: 'app-gestione-utente',
@@ -20,10 +20,10 @@ import { MatSelectModule } from '@angular/material/select';
 })
 export class GestioneUtente implements OnInit
 {
-  private readonly utenteS = inject(UtenteServices);
+  private readonly userS = inject(UtenteServices);
   private readonly util = inject(UtilitiesServices);
 
-  utenti = this.utenteS.accounts;
+  utenti = this.userS.accounts;
   isModifica = signal<boolean>(false);
   utenteCorrente: any = null;
 
@@ -37,11 +37,12 @@ export class GestioneUtente implements OnInit
     cognome: new FormControl(null, Validators.required),
     email: new FormControl(null, [Validators.required, Validators.email]),
     telefono: new FormControl(null, Validators.required),
-    ruolo: new FormControl(null, Validators.required)
+    ruolo: new FormControl(null, Validators.required),
+    username: new FormControl(null)
   })
 
   ngOnInit(): void {
-    this.utenteS.list();
+    this.userS.list();
   }
 
   get utentiFiltrati() {
@@ -84,12 +85,20 @@ export class GestioneUtente implements OnInit
     this.utenteCorrente = row;
     console.log("Utente corrente:", this.utenteCorrente);
     this.isModifica.set(true);
-    this.updateForm.patchValue({
-      nome: row.nome,
-      cognome: row.cognome,
-      email: row.email,
-      telefono: row.telefono,
-      ruolo: row.ruolo
+    this.userS.findByUserNameNumber(row.userId).subscribe({
+      next: (dettaglioUtente: any) => {
+        this.updateForm.patchValue({
+          nome: dettaglioUtente.nome,
+          cognome: dettaglioUtente.cognome,
+          email: dettaglioUtente.email,
+          telefono: dettaglioUtente.telefono,
+          ruolo: dettaglioUtente.ruolo,
+          username: dettaglioUtente.username 
+        });
+      },
+      error: (err) => {
+        console.error("Errore nel recupero dello username", err);
+      }
     });
   }
 
@@ -101,20 +110,47 @@ export class GestioneUtente implements OnInit
 
   salvaModifiche() {
     if (this.updateForm.valid) {
+      const formValues = this.updateForm.value;
 
       const bodyUpdate = {
         userId: this.utenteCorrente.userId, 
-        ...this.updateForm.value
+        nome: formValues.nome,
+        cognome: formValues.cognome,
+        email: formValues.email,
+        telefono: formValues.telefono
       };
 
-      this.utenteS.update(bodyUpdate).subscribe({
-        next: () => {
-          console.log("Aggiornamento riuscito");
-          this.isModifica.set(false);
-          this.utenteCorrente = null;
-          this.utenteS.list(); 
+      this.userS.update(bodyUpdate).subscribe({
+        next: () => 
+        {
+          const usernameCorrente = formValues.username;
+
+          if (formValues.ruolo && formValues.ruolo !== this.utenteCorrente.ruolo) 
+          {
+            this.userS.setRole(usernameCorrente, formValues.ruolo).subscribe({
+              next: () => 
+              {
+                console.log("Aggiornamento e ruolo riusciti");
+                this.isModifica.set(false);
+                this.utenteCorrente = null;
+                this.userS.list();
+              },
+              error: (err) => 
+              {
+                console.error("Errore durante l'aggiornamento del ruolo", err)
+              }
+            });
+          } 
+          else 
+          {
+            console.log("Aggiornamento riuscito");
+            this.isModifica.set(false);
+            this.utenteCorrente = null;
+            this.userS.list();
+          }
         },
-        error: (err) => {
+        error: (err) => 
+        {
           console.error("Errore durante l'update", err);
         }
       });
@@ -125,7 +161,7 @@ export class GestioneUtente implements OnInit
     console.log("Eliminazione di:", row);
     const idUtente = row.userId; 
 
-    this.utenteS.deleteUser(idUtente).subscribe({
+    this.userS.deleteUser(idUtente).subscribe({
       next: () => {
         console.log("Utente eliminato con successo");
         /*if (this.utenteCorrente && this.utenteCorrente.userId === idUtente) {
