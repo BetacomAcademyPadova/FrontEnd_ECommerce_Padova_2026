9d2b0a74a7ade647635a97772f4455d80ff8f219
simonestecca2003-ux <simonestecca2003@gmail.com>
2026-07-28 13:58:32 +0200

    Notifiche delete prodotto

--- src/app/componenti/notifiche/notifiche.ts
+++ src/app/componenti/notifiche/notifiche.ts
@@ -11,6 +11,7 @@ import { AuthServices } from '../../auth/auth-services';
 import { MatOptionModule } from '@angular/material/core';
 import { MatSelectModule } from '@angular/material/select';
 import { MatBadgeModule } from '@angular/material/badge';
+import { ProdottoServices } from '../../services/prodotto-services';
 
 @Component({
   selector: "app-notifiche",
@@ -23,18 +24,40 @@ import { MatBadgeModule } from '@angular/material/badge';
 export class Notifiche implements OnInit
 {
   private notificheService = inject(NotificheServices);
+  private prodottoService = inject(ProdottoServices);
   public auth = inject(AuthServices);
 
   notifiche = signal<any[]>([]);
+  prodottiList = signal<any[]>([]);
   msg = signal('');
   successMsg = signal('');
 
   richiestaForm: FormGroup = new FormGroup({
     tipoRichiesta: new FormControl('', Validators.required),
+    prodottoId: new FormControl(null),
     messaggio: new FormControl('', Validators.required)
   });
 
   ngOnInit(): void {
+    this.prodottoService.getAll().subscribe({
+      next: (res: any) => {
+        console.log("DATI PRODOTTO RICEVUTI:", res);
+        this.prodottiList.set(res);
+      },
+      error: () => {
+        this.msg.set("Errore caricamento prodotti");
+      }
+    });
+    this.richiestaForm.get('tipoRichiesta')?.valueChanges.subscribe(tipo => {
+      const prodottoCtrl = this.richiestaForm.get('prodottoId');
+      if (tipo === 'Rimuovere prodotto') {
+        prodottoCtrl?.setValidators([Validators.required]);
+      } else {
+        prodottoCtrl?.clearValidators();
+        prodottoCtrl?.setValue(null);
+      }
+      prodottoCtrl?.updateValueAndValidity();
+    });
     if (this.auth.grant().isAdmin) {
       this.notificheService.getTutteNonLette().subscribe({
         next: (res) => {
@@ -81,10 +104,14 @@ export class Notifiche implements OnInit
 
     const userId = Number(this.auth.grant().userId);
     const tipoRichiesta = this.richiestaForm.value.tipoRichiesta;
+    const prodottoId = this.richiestaForm.value.prodottoId;
     const messaggio = this.richiestaForm.value.messaggio;
 
     if (userId) {
-      const testoCompleto = `[${tipoRichiesta}] ${messaggio}`;
+      let testoCompleto = `[${tipoRichiesta}] - ${messaggio}`;
+      if (tipoRichiesta === 'Rimuovere prodotto' && prodottoId) {
+        testoCompleto = `[${tipoRichiesta}] - ID Prodotto: ${prodottoId} - ${messaggio}`;
+      }
       this.notificheService.inviaRichiesta(userId, testoCompleto).subscribe({
         next: () => {
           this.successMsg.set("Richiesta inviata con successo!");
@@ -102,12 +129,20 @@ export class Notifiche implements OnInit
     return testo.match(/ID:\s*(\d+)/)![1];
   }
 
+  estraiIdProdotto(testo: string): string {
+    return testo.match(/ID Prodotto:\s*(\d+)/)![1];
+  }
+
   estraiTipo(testo: string): string {
     return testo.match(/\[(.*?)\]/)![1];
   }
 
   estraiTesto(testo: string): string {
-     return testo.replace(/.*?\s*-\s*\[.*?\]\s*/, '');
+    return testo
+      .replace(/Richiesta ricevuta dall'utente ID:\s*\d+\s*-\s*/g, '') 
+      .replace(/\[.*?\]\s*-\s*/g, '')                                
+      .replace(/ID Prodotto:\s*\d+\s*-\s*/g, '')                      
+      .trim();
   }
 
   accettaRichiesta(idNotifica: number) {
@@ -131,7 +166,6 @@ export class Notifiche implements OnInit
       next: () => {
         this.notifiche.update(lista => {
           const nuovaLista = lista.filter(n => n.idNotifica !== idNotifica);
-          // Aggiorna il badge con il nuovo numero residuo
           this.notificheService.badgeCount.set(nuovaLista.length);
           return nuovaLista;
         });
