8cd5c9b7ad0a1b5aec9bc6d67bb9d3fd0fa7fe43
Vadim <49763091+Vrudim@users.noreply.github.com>
2026-07-31 12:24:14 +0200

    Delete auth directory

--- auth/admin-guard.ts
+++
@@ -1,9 +0,0 @@
-import { CanActivateFn } from '@angular/router';
-import { AuthServices } from './auth-services';
-import { inject } from '@angular/core';
-
-export const adminGuard: CanActivateFn = (route, state) => {
-    const authServices = inject(AuthServices);
-
-  return authServices.isRoleAdmin();
-};
