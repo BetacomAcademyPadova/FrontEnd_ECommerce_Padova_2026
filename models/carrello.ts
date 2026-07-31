399d9b18fd0ad10a1c1fba9f8a2e8fc5f4d033a3
Vadim <49763091+Vrudim@users.noreply.github.com>
2026-07-31 12:24:23 +0200

    Delete models directory

--- models/carrello.ts
+++
@@ -1,9 +0,0 @@
-import { ProdottiCarrello } from "./prodotti-carrello";
-
-export interface Carrello {
-    idCarrello: number;
-    dataUltimoAgg: string;
-    user: any;
-    totale: number;
-    prodotti: ProdottiCarrello[];
-}
--- models/prodotti-carrello.ts
+++
@@ -1,8 +0,0 @@
-export interface ProdottiCarrello {
-    idRiga: number;
-    idCarrello: number;
-    idDivisioneProdotto: number;
-    quantita: number;
-    prezzo: number;
-    subtotale: number;
-}
